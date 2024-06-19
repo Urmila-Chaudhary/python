0b81e813d95f3049537abf1de8c6a7e3b6adf4b2
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-19 14:08:12 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,16 @@
+names = ["Urmila","Gracen","Varghese","Sreekesh","Sreekumar"]
+"""#if-else condition
+# on the margin - global scope
+#indentation should be uniform in that particular block/scope
+if "Gracen" in names:
+    print("Gracen is in the list")
+else:
+    print("Gracen is not in the list")
+names.sort()
+#for loop
+for x in names:
+    print(x)
+i = 0
+while i < len(names):
+    print(names[i])
+    i+=1"""
